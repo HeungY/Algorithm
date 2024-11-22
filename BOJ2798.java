4476ce6d2f414c4687afb05330652a998f22caf9
HeungY <rkrkrkrk02@naver.com>
2024-11-22 17:15:32 +0900

    feat: 백준 2798 블랙잭
    Brute Force

---
+++ BOJ2798.java
@@ -0,0 +1,18 @@
+
+//  블랙잭
+
+/*
+    첫째 줄에 N 과 M, 둘째 줄에 N개의 숫자만큼 카드의 점수 주어짐.
+    N은 카드 갯수, M은 목표 점수
+
+    M을 넘지 않는 3장의 카드 조합의 최댓값 구하기.
+
+
+ */
+
+
+public class BOJ2798 {
+    public static void main(String[] args) {
+
+    }
+}
