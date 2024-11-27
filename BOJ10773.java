e460c3137c2b1e1d096b589622961fe9b9fc5906
HeungY <rkrkrkrk02@naver.com>
2024-11-27 23:18:07 +0900

    feat: 백준 10773 제로
    구현

---
+++ BOJ10773.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Stack;
+
+public class BOJ10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        System.out.println(zero(stack));
+
+    }
+
+    static BigInteger zero(Stack<Integer> stack) {
+        BigInteger count = BigInteger.ZERO;
+
+        while (!stack.isEmpty()) {
+            BigInteger stackInt = BigInteger.valueOf(stack.pop());
+            count = count.add(stackInt);
+        }
+
+        return count;
+    }
+}
